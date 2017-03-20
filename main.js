aac5b72bd63e52eb99824c4dd7c457d31e9f7954
KrystineJannenga <kjannenga1@gmail.com>
2017-03-20 09:08:06 -0700

    initial commit

---
+++ main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $.getJSON('https://api.etsy.com/v2/listings/active.js?api_key=h9oq2yf3twf4ziejn10b717i&keywords=whiskey&includes=Images,Shop&callback=?', function(data){
+		console.log(data)
+	})
+
+	
+
+})
+
+
+
+
+
